e4b6993df1d515f5b70be4176fa0c18c63d388d1
60102 <leechenglok@hotmail.com>
2020-08-03 18:47:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/hellotoast/MainActivity2.java
@@ -0,0 +1,19 @@
+package com.example.android.hellotoast;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        String count = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView display = findViewById(R.id.textView2);
+        display.setText(count);
+    }
+}
